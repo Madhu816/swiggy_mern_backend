ca05b0d2d80f14fed586880fa66ac9108e4b0267
Madhu816 <madhupiske816@gmail.com>
2025-06-13 19:13:40 +0530

    Updated index.js and added new uploads

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 
 // app.use(cors({
-//     origin:"http://localhost:5173"
+//     origin:"http://localhost:5174"
 // }))
 
 mongoose.connect(process.env.MONGO_URL)
